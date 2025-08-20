8fd625eaf5fe707cf36c37041680dc979683363a
Imam Sya'bana <imambritish@gmail.com>
2025-08-20 11:44:15 +0700

    hosting the app

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("FLASK_KEY", os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'postgres://neondb_owner:npg_0nqtbHFOEG7P@ep-morning-queen-adhe20kk-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
